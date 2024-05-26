7353f2f0e5bdfa939e5accbb0e213fdb4c23aa93
Quan Do <biquando@gmail.com>
2024-05-25 17:31:05 -0700

    Clean up set_path function

--- functions/__fish_user_set_path.fish
+++ functions/__fish_user_set_path.fish
@@ -1,14 +1,17 @@
 function __fish_user_set_path
     switch $_UNAMESYS
         case 'Darwin'; set pathlist \
+            $HOME/bin \
+            $HOME/.local/bin \
             $HOME/.cargo/bin \
             /opt/homebrew/bin \
             /opt/homebrew/sbin \
             /Applications/Docker.app/Contents/Resources/bin
         case 'Linux'; set pathlist \
+            $HOME/bin \
+            $HOME/.local/bin \
             $HOME/services/bin
     end
 
-    fish_add_path -g $HOME/bin $HOME/.local/bin
     fish_add_path -g $pathlist
 end
